7dad0ca661f7a935492335a6dbfbd919013fcf2e
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-06 16:12:02 +0700

    set up structure

---
+++ src/components/List/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <div>
+            <h3>This is List</h3>
+        </div>
+    );
+};
+
+export default List;
---
+++ src/components/PlaceDetails/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlaceDetails = () => {
+    return (
+        <div>
+            <h3>This is PlaceDetails</h3>
+        </div>
+    );
+};
+
+export default PlaceDetails;
