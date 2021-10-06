ad04decd8242807f361fa49c06b9ae459b24ffd8
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-06 16:37:20 +0700

    finish header and map display

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+
+import useStyles from './style';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width: 600px)');
+
+    const coordinate = { lat: 0, lng: 0 }
+
     return (
-        <div>
-            <h3>This is Map</h3>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={coordinate}
+                center={coordinate}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+
+            >
+
+            </GoogleMapReact>
+
         </div>
     );
 };
